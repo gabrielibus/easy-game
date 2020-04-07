854039e8ed4807f057a910c568124aa12dd89c6c
gabriel <gabo.lazos@gmail.com>
2020-04-06 23:19:49 -0500

    adding items

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { useState } from "react"
 import "./styles/App.css"
 import defaultStats from "./models/stats.json"
-import actions from "./actions/actions.js"
+// import actions from "./actions/actions.js"
 
 /* STEPS! 
   1. Create a json with initial stats
-  2. Create states for players and scores
-  3. Create Divs structure for playes score
+  2. Create states: players, agresor, defender, turns
+  3. Create Divs structure for playes scores
   4. Create Divs sctructure for actions menu
   5. Creacte actions:
-    a. attack: 
-
-
+  6. style css
 */
 
 function App() {
@@ -22,50 +20,80 @@ function App() {
   const [defender, setDefender] = useState("playerTwo")
   const [turns, setTurns] = useState(1)
   const players = { playerOne, playerTwo }
-
-  let entries = (obj) => {
-    return Object.entries(obj)
-  }
-
-  let switchPlayers = () => {
-    setAggressor(defender)
-    setDefender(agresor)
-  }
-
-  let updateStat = (key, value) => {
-    agresor === "playerOne"
+  
+  let actions = {
+    entries: (obj) => {
+      return Object.entries(obj)
+    },
+    attackHp: () => {
+      let damage = players[agresor].hp - players[defender].def
+      let newHp = players[defender].hp
+      damage > 0
+      ? actions.updateDefenderStat("hp", newHp - damage)
+      : actions.updateDefenderStat("hp", newHp - 1)
+      actions.finishTurn()
+    },
+    attackDef: () => {
+      let newDef = players[defender].def
+      actions.updateDefenderStat("def", newDef - 1)
+      actions.finishTurn()
+    },
+    finishTurn: () => {
+      setTurns(turns + 1)
+      actions.switchPlayers()
+    },
+    // items: (name) => {
+      //   let agresorHp = players[agresor].hp
+      //   let agresorDef = players[agresor].def
+      //   name[0] === 'cure'
+      //   ? items.cure(agresorHp)
+      //   : items.fixArmor(agresorDef)
+      //   // idx === 0 ? up
+      
+      //   actions.updateAgresorStat('def', agresorHp + 2)
+    //   actions.finishTurn()
+    // },
+    switchPlayers: () => {
+      setAggressor(defender)
+      setDefender(agresor)
+    },
+    updateAgresorStat: (key, value) => {
+      agresor === "playerOne"
+      ? setPlayerOne({ ...playerOne, [key]: value })
+      : setPlayerTwo({ ...playerTwo, [key]: value })
+    },
+    updateDefenderStat: (key, value) => {
+      agresor === "playerOne"
       ? setPlayerTwo({ ...playerTwo, [key]: value })
       : setPlayerOne({ ...playerOne, [key]: value })
+    },
+    helpItem: (itemArray) => {
+      let method = itemArray[0]
+      items[method]()
+    }
   }
-
-  let attackHp = () => {
-    let damage = Math.abs(players[agresor].hp - players[defender].def)
-    let newHp = players[defender].hp - damage
-    damage > 0 ? updateStat("hp", newHp) : updateStat("hp", newHp - 1)
-    finishTurn()
-  }
-
-  let attackDef = () => {
-    let newDef = players[defender].def - 1
-    updateStat("def", newDef)
-    finishTurn()
-  }
-
-  let items = (idx) => {alert(idx+1)}
-
-  let finishTurn = () => {
-    setTurns(turns + 1)
-    switchPlayers()
-  }
-
+  
+    let items = {
+      cure: () => {
+        const agresorHp = players[agresor].hp
+        actions.updateAgresorStat('hp', agresorHp + 2)
+        actions.finishTurn()
+      },
+      fixArmor: () => {
+        const agresorDef = players[agresor].def
+        actions.updateAgresorStat('def', agresorDef + 2)
+        actions.finishTurn()
+      }
+    }
+  
   return (
     <div className='App'>
       <h1>my turns based game</h1>
       <h5>turn {turns}</h5>
       <div className='arenaWrapper'>
-        <div className='playerOne'>
-          {entries(playerOne).map((e, index) => (
-            <table>
+        <div className='playerOne' id={agresor === 'playerOne' ? 'active' : ''}>
+          {actions.entries(playerOne).map((e, idx) => (
+            <table key={idx}>
               <thead>
                 <tr>
                   <th>{e[0]}</th>
@@ -75,9 +103,9 @@ function App() {
             </table>
           ))}
         </div>
-        <div className='playerTwo'>
-          {entries(playerTwo).map((e, index) => (
-            <table>
+        <div className='playerTwo' id={agresor === 'playerTwo' ? 'active' : ''}>
+          {actions.entries(playerTwo).map((e, idx) => (
+            <table key={idx}>
               <thead>
                 <tr>
                   <th>{e[0]}</th>
@@ -90,21 +118,56 @@ function App() {
       </div>
       <div className='actionsWrapper'>
         <h3> {agresor} turn </h3>
-        <button onClick={() => attackHp()}>atacar</button>
-        <button onClick={() => attackDef()}>romper armadura</button>
-        <button>magia</button>
+        <button onClick={() => actions.attackHp()}>atacar</button>
+        <button onClick={() => actions.attackDef()}>romper armadura</button>
+        <button>habilidades</button>
         <div className='items'>
           <h5>items</h5>
           {players[agresor].items.map((e, idx) => (
-            <button key={idx} onClick={() => items(idx)}>
+            <button key={idx} onClick={() => actions.helpItem(e)}>
               {e[0]}: {e[1]}
             </button>
           ))}
         </div>
-        <button onClick={() => finishTurn()}>finalizar</button>
+        <button onClick={() => actions.finishTurn()}>finalizar</button>
       </div>
     </div>
   )
 }
 
 export default App
+
+// let entries = (obj) => {
+//   return Object.entries(obj)
+// }
+
+// let switchPlayers = () => {
+//   setAggressor(defender)
+//   setDefender(agresor)
+// }
+
+// let updateDefStat = (key, value) => {
+//   agresor === "playerOne"
+//     ? setPlayerTwo({ ...playerTwo, [key]: value })
+//     : setPlayerOne({ ...playerOne, [key]: value })
+// }
+
+// let attackHp = () => {
+//   let damage = Math.abs(players[agresor].hp - players[defender].def)
+//   let newHp = players[defender].hp - damage
+//   damage > 0 ? updateDefStat("hp", newHp) : updateDefStat("hp", newHp - 1)
+//   finishTurn()
+// }
+
+// let attackDef = () => {
+//   let newDef = players[defender].def - 1
+//   updateDefStat("def", newDef)
+//   finishTurn()
+// }
+
+// let items = (idx) => {alert(idx+1)}
+
+// let finishTurn = () => {
+//   setTurns(turns + 1)
+//   switchPlayers()
+// }
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,38 @@
-let actions = {
 
-}
+
+let actions = {
+    entries: (obj) => {
+      return Object.entries(obj)
+    },
+    attackHp: () => {
+      let damage = players[agresor].hp - players[defender].def
+      let newHp = players[defender].hp
+      damage > 0
+        ? actions.updateStat("hp", newHp - damage)
+        : actions.updateStat("hp", newHp - 1)
+      actions.finishTurn()
+    },
+    attackDef: () => {
+      let newDef = players[defender].def - 1
+      actions.updateStat("def", newDef)
+      actions.finishTurn()
+    },
+    finishTurn: () => {
+      setTurns(turns + 1)
+      actions.switchPlayers()
+    },
+    items: (idx) => {
+      alert(idx + 1)
+    },
+    switchPlayers: () => {
+      setAggressor(defender)
+      setDefender(agresor)
+    },
+    updateStat: (key, value) => {
+      agresor === "playerOne"
+        ? setPlayerTwo({ ...playerTwo, [key]: value })
+        : setPlayerOne({ ...playerOne, [key]: value })
+    },
+  }
 
 module.exports = actions
